c7a5bf3e85f9180297f47a1c0d9204ef0ed94f71
Gustaf Räntilä <g.rantila@gmail.com>
2020-02-07 15:36:12 +0100

    No /oauth2 for logout, seems unnecessary nowadays

--- packages/oidc-middleware/src/logout.js
+++ packages/oidc-middleware/src/logout.js
@@ -69,7 +69,7 @@ logout.forceLogoutAndRevoke = context => {
     };
 
     // redirect to Okta to clear SSO session
-    const endOktaSessionEndpoint = `${issuer}/oauth2/v1/logout?${querystring.stringify(params)}`;
+    const endOktaSessionEndpoint = `${issuer}/v1/logout?${querystring.stringify(params)}`;
     return res.redirect(endOktaSessionEndpoint);
   };
 };
